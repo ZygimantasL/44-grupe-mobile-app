b2cf1f16ce8bd53eea0cb07a7f02bb81b69666e4
ZygimantasL <129725364+ZygimantasL@users.noreply.github.com>
2023-07-24 13:31:51 +0300

    update before style

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import style from  './App.module.css';
+
+function App() {
+    const [name, setName] = useState('');;
+    const [surname, setSurname]= useState('');
+    function updateName(event) {
+        setName(event.target.value);
+    }
+
+    //DOM.addEventListener('click', (event)=> {/* ... */});
+    return(
+        <div className={style.app}>
+            <form className={style.form}>
+                <div className={style.row}>
+                <label htmlFor='name'>Name</label>
+                <input onChange={updateName} value={name} id='name' type="text" required placeholder='Your name here' />
+                </div>
+                <div className={style.row}>
+                <label htmlFor='surname'>Surame</label>
+                <input onChange={(e)=> setSurname(e.target.value )} id='surname' type="text" required placeholder='Your surname here' />
+                </div>
+                <div className={style.row}>
+                <button type='submit'>Create</button>
+                </div>
+            </form>'
+            <div className={style.preview}>"{`${name} ${surname}`.trim() }"</div>
+        </div>
+    )
+    
+}
+export default App;
